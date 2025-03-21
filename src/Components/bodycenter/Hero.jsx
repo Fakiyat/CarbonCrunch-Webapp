5d620a5f0265ff811772713dd4627945dbd22ff5
Fakiyat <fakiyatafaq14@gmail.com>
2025-03-22 03:30:25 +0530

    First commit

---
+++ src/Components/bodycenter/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <section className="hero-main">
+      <div className="hero-container">
+        <div className="colored-bar">
+          <img src="./src/images/Text.png" alt="hand-icon" />
+          <span className="colored-text"> Let's have a tour </span>
+        </div>
+        <div className="hero-content">
+          <h1 className="hero-heading">
+            Streamline Your Sustainability Reporting With
+            <span className="highlight"> CARBON CRUNCH</span>
+          </h1>
+          <p className="hero-subheading">
+            <span className="highlight">90% </span> With Accurate Carbon
+            Calculations Trusted by Industry Leaders
+          </p>
+          <div className="hero-btn">
+            <button className="calculator-btn"> Free Calculator</button>
+            <button className="book-demo-btn"> Book Demo</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
